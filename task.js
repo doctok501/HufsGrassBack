dba429245a2bd7bc850b7f6816553b29921649e0
ShinHyeongcheol <65026631+ShinHyeongcheol@users.noreply.github.com>
2022-01-10 03:01:18 +0900

    Delete Kakaowork

--- task.js
+++ task.js
@@ -1,19 +1,15 @@
 const { getLastSavedCommitTime, saveCommit } = require('./api/db');
-const { sendTodayResult, sendYesterdayResult } = require('./api/kakaowork');
 const { getUnsavedCommit } = require('./api/slack');
 const { member_list_github } = require('./config/config');
 
 switch (process.argv[2]) {
-  case 'today':
-    sendTodayResult();
-    break;
-  case 'yesterday':
-    sendYesterdayResult();
-    break;
   case 'save':
     (async () => {
       const lastTime = await getLastSavedCommitTime().then((res) => res);
       let unSavedCommit = await getUnsavedCommit(lastTime);
+	  console.log(lastTime);
+	  console.log(unSavedCommit);
+	  console.log(member_list_github);
       unSavedCommit = unSavedCommit.reverse();
       unSavedCommit.map(async (e) => {
         if (!member_list_github.includes(e['author_name'])) {
