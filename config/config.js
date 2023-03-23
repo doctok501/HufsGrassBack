20ec4d0abd7045a3f1d640f1577d224837e99b39
kim_ingyeom <doctok501@gmail.com>
2023-03-23 16:04:53 +0900

    Hufs Garden

--- config/config.js
+++ config/config.js
@@ -10,8 +10,17 @@ CONFIG.db_name = process.env.DB_NAME || 'name';
 CONFIG.db_user = process.env.DB_USER || 'root';
 CONFIG.db_password = process.env.DB_PASSWORD || 'db-password';
 
-CONFIG.member_list = process.env.MEMBER_LIST.split(',') || [];
-CONFIG.member_list_github = process.env.MEMBER_LIST_GITHUB.split(',') || [];
+if (process.env.MEMBER_LIST && typeof process.env.MEMBER_LIST === 'string') {
+    CONFIG.member_list = process.env.MEMBER_LIST.split(',');
+  } else {
+    CONFIG.member_list = [];
+  }
+  
+if (process.env.MEMBER_LIST_GITHUB && typeof process.env.MEMBER_LIST_GITHUB === 'string'){
+    CONFIG.member_list_github = process.env.MEMBER_LIST_GITHUB.split(',');
+} else{
+    CONFIG.member_list_github = [];
+}
 
 CONFIG.slack_api_token = process.env.SLACK_API_TOKEN;
 CONFIG.slack_channel_id = process.env.SLACK_CHANNEL_ID;
